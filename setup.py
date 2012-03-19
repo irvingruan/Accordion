1b71b2df6c0d0cb50ebb2328251bb9cd49214043
Irving Ruan <irvingruan@gmail.com>
2012-03-19 00:14:37 -0700

    Changed project website to github page, old site outdate

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version = "0.5",
     author = "Irving Ruan",
     author_email = "irvingruan@gmail.com",
-    url = "http://www.liquidx.net/pytagger/",
+    url = "http://github.com/irvingruan/Accordion/",
 	description = "Python ID3 Tag Reader and Writer Module",
 	long_description = "An ID3v1 and ID3v2 tag reader and writer module in pure Python. Supports all standards including ID3v1, ID3v1.1, ID3v2.2, ID3v2.3 and ID3v2.4",
 	license = "BSD",
